6accdc981aff9a2e540fea7d09f9b42e5dd853b1
Kenny Boateng <kennyboateng.99@gmail.com>
2025-02-06 18:59:48 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "/https://surprised-asp-kennycodes-72574429.koyeb.app";
+const BASE_URL = "https://surprised-asp-kennycodes-72574429.koyeb.app";
 
 export default BASE_URL;
 // http://localhost:3001
